709f6c1974021915d9a49acc65bf976bdf9a33f7
ct-cybersec <80648240+ct-cybersec@users.noreply.github.com>
2023-01-20 16:29:42 -0500

    Update pass-gen.py

--- pass-gen.py
+++ pass-gen.py
@@ -1,10 +1,31 @@
-#very simple python script to print out a random string of 16 characters for a password
 import random
 import string
+import sys
+import os
 
-letters = string.ascii_letters
-numbers = string.digits
-punctuation = "$!&%@"
-together = letters + numbers + punctuation
+try:
+	password = int(sys.argv[1])
+except TypeError:
+	print("\033[1;38;2;255;0;0mDon't be silly, it has to be an integer (number)\033[0m 🤦")
+	sys.exit(1)
+except IndexError:
+	print("\033[1;38;2;255;0;0mDon't be silly, input something, at least!\033[0m 🤦")
+	sys.exit(1)
+except ValueError:
+	print("\033[1;38;2;255;0;0mDon't be silly, it has to be an integer (number)\033[0m 🤦")
+	sys.exit(1)
 
-print(''.join(random.choices(together,k=16)))
+if password >= 1:
+	if (password <= 50) & (password >= 6):
+	    password_length = password
+	    letters = string.ascii_letters
+	    numbers = string.digits
+	    punctuation = "$!&%@.,#?"
+	    together = letters + numbers + punctuation
+	    print(''.join(random.choices(together,k=password_length)))
+	if password > 50:
+		print("\033[1;38;2;255;0;0mPlease provide a reasonable password length (i.e. 6-50)\033[0m 🤦")
+	if password < 6:
+		print("\033[1;38;2;255;0;0mCome on, you shouldn't be using a password that's less than 6... try again\033[0m 🤦")
+else:
+	print("\033[1;38;2;255;0;0mPlease try again by providing an integer value. Run the program as such:\033[0m\n\033[1mpython pass-gen.py 10\033[0m")
